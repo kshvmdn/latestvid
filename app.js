2f44f58b3c606f93fb3e8a41f285c109a0c4ccbb
Kashav Madan <kshvmdn@gmail.com>
2016-01-10 15:58:22 -0500

    Added native ES6 Promises + error msg if no vid/nonexistent user

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const Xray = require("x-ray"), xray = Xray();
-
-var base_url = "https://www.youtube.com/user/" + process.argv[2] + "/videos";
-
-xray(base_url, "li.channels-content-item a@href")(function(err, body) {
-  console.log(body);
-});
---
+++ index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+"use strict";
+
+const Xray = require("x-ray"), xray = Xray();
+const open = require("open");
+// const Promise = require("bluebird");
+
+var getLatestVid = function(user) {
+  var base = "https://www.youtube.com/user/" + user + "/videos";
+  var prom = new Promise(function(resolve, reject) {
+    xray(base, "li.channels-content-item a@href")(function(err, body) {
+      if (!err && body != undefined)
+        resolve(body);
+      else
+        reject(err);
+    });
+  });
+  prom.then(function(url){
+    console.log("Opening latest upload by user \"" + user + "\"...");
+  }).catch(function() {
+    console.log("User \"" + user + "\" doesn\'t exist or has no videos.");
+  });
+}
+
+getLatestVid(process.argv[2]);
