8411ca95a930e2419e4b7f34fb020d470053cc78
Kashav Madan <kshvmdn@gmail.com>
2016-01-10 16:08:57 -0500

    Refactored for bluebird promises

--- index.js
+++ index.js
@@ -3,24 +3,23 @@
 
 const Xray = require("x-ray"), xray = Xray();
 const open = require("open");
-// const Promise = require("bluebird");
+const Promise = require("bluebird");
 
 var getLatestVid = function(user) {
-  var base = "https://www.youtube.com/user/" + user + "/videos";
-  var prom = new Promise(function(resolve, reject) {
+  let base = "https://www.youtube.com/user/" + user + "/videos";
+  return new Promise(function(resolve, reject) {
     xray(base, "li.channels-content-item a@href")(function(err, body) {
-      if (!err && body != undefined)
-        resolve(body);
-      else
-        reject(err);
+      if (err || body == undefined)
+        reject();
+      resolve(body);
     });
-  });
-  prom.then(function(url){
-    console.log("Opening latest upload by user \"" + user + "\"...");
-    open(url);
-  }).catch(function() {
-    console.log("User \"" + user + "\" doesn\'t exist or has no videos.");
   });
 }
 
-getLatestVid(process.argv[2]);
+let ytUser = process.argv[2] != undefined ? process.argv[2] : 'marquesbrownlee';
+getLatestVid(ytUser).then(function(url) {
+  console.log("Opening latest " + ytUser.toUpperCase() + " video...");
+  open(url);
+}).catch(function(e) {
+    console.log("User " + ytUser.toUpperCase() + " doesn\'t exist or has no videos.");
+});
