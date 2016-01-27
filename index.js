d30b18c70e59e4aba7b0f8b7b87ae6d3de0af135
Kashav Madan <kshvmdn@gmail.com>
2016-01-27 00:23:32 -0500

    Add func. for dl

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const open = require("open");
 const Promise = require("bluebird");
 const fs = require('fs');
 const ytdl = require('youtube-dl');
+const path = require('path');
 
 var getLatestVid = function(user) {
   let base = "https://www.youtube.com/user/" + user + "/videos";
@@ -18,14 +19,42 @@ var getLatestVid = function(user) {
   });
 }
 
+var openVid = function(url) {
+  console.log("Opening the latest " + ytUser.toUpperCase() + " video...");
+  open(url);
+}
+
+var dlVid = function(url) {
+  console.log("Downloading the latest " + ytUser.toUpperCase() + " video...");
+  var video = ytdl(url, ['-f', '22']); // 1080p mp4
+  var size = 0;
+  video.on('info', function(info) {
+    console.log(info);
+    size = info.size;
+    var file = path.join('~/Desktop', info._filename);
+    video.pipe(fs.createWriteStream(file));
+  });
+
+  var pos = 0;
+  video.on('data', function data(chunk) {
+    pos += chunk.length;
+    if (size) {
+      var percent = (pos / size * 100).toFixed(2);
+      process.stdout.cursorTo(0);
+      process.stdout.clearLine(1);
+      process.stdout.write(percent + '%');
+    }
+  });
+  video.on('end', function end() {
+    console.log('\n');
+  })
+}
+
 let ytUser = process.argv[2] != undefined ? process.argv[2] : 'marquesbrownlee';
 let download = Boolean(process.argv[3]);
 
 getLatestVid(ytUser).then(function(url) {
-  if (!download) {
-    console.log("Opening the latest " + ytUser.toUpperCase() + " video...");
-    open(url);
-  }
+  return (download) ? dlVid(url) : openVid(url);
   process.exit(0);
 }).catch(function(e) {
   console.log("User " + ytUser.toUpperCase() + " doesn\'t exist or has no videos.");
