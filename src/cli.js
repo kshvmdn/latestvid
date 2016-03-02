14702412aed33bb1d22f2644599f92b7c77e006d
Kashav Madan <kshvmdn@gmail.com>
2016-03-01 22:16:51 -0500

    Add validation, exit to cli

--- src/cli.js
+++ src/cli.js
@@ -18,13 +18,27 @@ const cli = meow(`
 
 const flags = cli.flags;
 
+let error = {
+  'msg': null,
+  'code': 0
+};
 
+const exit = function(msg) {
+  error.msg = msg;
+  error.code = 1;
 };
 
 const exists = function(var_) {
   return var_ !== undefined && typeof(var_) === 'boolean' && String(var_) !== '';
 };
 
+if (exists(flags['h'])) {
+  exit(`${cli.help}`);
+} else if (exists(flags['v'])) {
+  exit(`latestvid v${version}`);
+} else if (exists(flags['u']) || exists(flags['user'])) {
+  exit(`Expected user account, run 'latestvid -h' for help.`);
+}
 
 let username = flags['u'] || flags['user'];
 let download = Boolean(flags['d'] || flags['download']);
@@ -32,4 +46,5 @@ let download = Boolean(flags['d'] || flags['download']);
 module.exports = {
   'user': username,
   'toDl': download,
+  'error': error
 };
